81695088ae06737c202973c08643de976f612ea5
dhiru2498 <113499167+dhiru2498@users.noreply.github.com>
2022-09-22 21:05:00 +0530

    Add files via upload

--- Emp11.java
+++ Emp11.java
@@ -1,53 +1,54 @@
 package com.codebind;
 
-
-public class Emp2 {
-	
-	    int id;
-	    String name, address;
-	    String phone;
-	     Emp2(int i, String n, String a, String p){
-	        this.id = i;
-	        this.name = n;
-	        this.address = a;
-	        this.phone = p;
-	    }
-
-	    public void display(int i){
-	        System.out.println("Name:"+id+"\nphone:"+phone);
-	    }
-
-	    public void display(){
-	        System.out.println("\nName:"+name+"\nAddress:"+address);
-	    }
+public class Emp11 {
+	 int id;  
+	 String name, address;
+	 String phone;
+	 String type;
 	    
-	    public static void main(String args[]){
-
-	        Emp2 obj1 = new Emp2(101,"Dhiraj","Bihar", "9123265302");
-	        HR obj2 = new HR(101,"Dhiraj","Bihar", "9123265302", 35000);
-	        obj1.display(1);
-	        obj1.display();
-	        obj2.empWithSalary();
-	        obj2.empWithSalary(3000);
-	    }
-
+	Emp11(int i, String n, String a, String p,String t){
+        this.id = i;
+        this.name = n;
+        this.address = a;
+        this.phone = p;
+        this.type=t;
+    }
+	public void testdm() {
+		if(type.equals("Hr"))
+		{
+			System.out.println("Inside Hr");
+			
+		}
+		else if(type.equals("admin"))
+		{
+			System.out.println("Inside admin");
+		}
+		else
+		{
+			System.out.println("infra");
+		}
 	}
 
- class HR extends Emp2{
-	    int salary;
-	    public HR(int i, String n, String a, String p, int sal) {
-	        super(i, n, a, p);
-	        this.salary = sal;
-	    }
 
-	    public void empWithSalary(){
-	        System.out.println("\nEmployee Name: "+name+"\nSalary: "+salary);
-	    }
+   public void display(int i){
+        System.out.println("Name:"+id+"\nphone:"+phone);
+    }
 
-	    public void empWithSalary(int tax){
-	        System.out.println("\nEmployee Name: "+name+"\nSalary: "+(salary-tax)+"\nTax Deducted: "+tax);
-	    }
-	    
-	}
 
+   public void display(){
+        System.out.println("\nName:"+name+"\nAddress:"+address);
+    }
+   
+   
+   public static void main(String[] args) {
+	   Emp11 obj = new Emp11(101, "Dhiraj", "Bihar" ,"9123265302","Hr");
+	   Emp11 obj1=new Emp11(101, "Dhiraj", "Bihar" ,"9123265302","admin");
+	   Emp11 obj2=new Emp11(101, "Dhiraj", "Bihar" ,"9123265302","infra");
+	   
+	   obj.testdm();
+	   obj1.testdm();
+	   obj2.testdm();
+   }
+   
+}
 
