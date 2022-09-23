d90abcaf6713504f65da2dcf37865753bab6cf16
dhiru2498 <113499167+dhiru2498@users.noreply.github.com>
2022-09-23 16:27:59 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,14 @@
+package com.codebind;
+
+public class Substring {
+	public static void main(String[] args) {
+String s="I have joined Amantya at 01-09-2022";
+
+int fi = s.lastIndexOf(" ");
+System.out.println(s.substring(fi, fi+3));
+
+
+
+
+}
+}
