79723959204a78492749088c7417efa121cfa67a
dhiru2498 <113499167+dhiru2498@users.noreply.github.com>
2022-09-23 16:50:35 +0530

    Add files via upload

---
+++ Reversestring.java
@@ -0,0 +1,18 @@
+package com.codebind;
+
+public class Reversestring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Dhiraj";
+		for(int i=s.length();i>0;i--)
+		{
+			char d=s.charAt(i-1);
+			System.out.print(d+ " ");
+			
+		}
+
+	}
+
+}
+
