00236d25acfcc29be8072fda9dd61956eb240069
dhiru2498 <113499167+dhiru2498@users.noreply.github.com>
2022-09-27 16:53:22 +0530

    Add files via upload

---
+++ Stringthrow.java
@@ -0,0 +1,18 @@
+package com.codebind;
+
+public class Stringthrow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=22;
+		if(age<22) {
+			throw new ArithmeticException("yes");
+			
+		}
+		else {
+			System.out.println("no");
+		}
+
+	}
+
+}
