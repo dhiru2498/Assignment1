19a63a81de9966fdbed786b753c4bc73f4944652
dhiru2498 <113499167+dhiru2498@users.noreply.github.com>
2022-10-10 12:16:55 +0530

    Add files via upload

---
+++ TestTreeSet.java
@@ -0,0 +1,20 @@
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class TestTreeSet {
+
+    public static void main(String args[]){
+        TreeSet<String> treeSet = new TreeSet<>();
+
+        treeSet.add("Rahul");
+        treeSet.add("Dhiraj");
+        treeSet.add("kumar");
+        treeSet.add("ravi");
+        treeSet.add("rohit");
+        treeSet.add("saurav");
+        treeSet.add("Aman");
+
+        System.out.println(treeSet);
+    }
+
+}
